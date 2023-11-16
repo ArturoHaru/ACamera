5047d9078f35f63e3128d65013a434f197be2af5
Arthur <41858848+ArturoHaru@users.noreply.github.com>
2023-11-16 16:59:08 +0100

    Did bottom UI

--- ACamera/ContentView.swift
+++ ACamera/ContentView.swift
@@ -9,9 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Color.black
-            .ignoresSafeArea()
-        
+        ZStack{
+            Color.black
+                .ignoresSafeArea()
+            VStack{
+                Spacer()
+                HStack{
+                    Rectangle()
+                        .fill(.secondary)
+                        .frame(width: 50,height: 50)
+                    Spacer()
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        ZStack{
+                            
+                            Circle()
+                                .fill(.white)
+                                .frame(width: 70, height: 70)
+                            Circle()
+                                .stroke(Color.white, lineWidth: 4)
+                                .frame(width: 80, height: 80)
+                            
+                        }
+                        
+                    })
+                    Spacer()
+                    ZStack{
+                        Circle()
+                            .fill(.secondary)
+                            .frame(width: 50,height: 50)
+                        Image(systemName: "arrow.triangle.2.circlepath")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 35,height: 35)
+                            .foregroundStyle(.white)
+                        
+                    }
+                    
+                }
+                .padding()
+            }
+        }
     }
 }
 
