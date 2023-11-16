5e4a889f105e67534f7387f2db93387ac0476080
Arthur <41858848+ArturoHaru@users.noreply.github.com>
2023-11-16 16:34:59 +0100

    Initial Commit

---
+++ ACamera/ACameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ACameraApp.swift
+//  ACamera
+//
+//  Created by Arturo Cecora on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ACameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
